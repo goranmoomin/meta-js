5201dbe711ead2deeecd0566023efd8e7cceab7e
Sungbin Jo <pcr910303@icloud.com>
2019-10-09 00:17:08 +0900

    Refactor expression enforestation

--- src/enforester.js
+++ src/enforester.js
@@ -14,13 +14,13 @@ const {
     isOperator
 } = require("./operator-utils.js");
 
-const EXPR_LOOP_OPERATOR = "EXPR_LOOP_OPERATOR";
-const EXPR_LOOP_NO_CHANGE = "EXPR_LOOP_NO_CHANGE";
-const EXPR_LOOP_EXPANSION = "EXPR_LOOP_EXPANSION";
-
 class Enforester {
     constructor(terms) {
         this.terms = terms;
+        this.term = null; // term the enforester is working on
+        this.operators = []; // the operator stack
+        // this.operators should not be manipulated outside
+        // of this.enforestExpressionLoop()
     }
 
     peek(n = 0) {
@@ -31,6 +31,15 @@ class Enforester {
         const term = this.terms[0];
         this.terms = this.terms.slice(1);
         return term;
+    }
+
+    get operator() {
+        // TODO: saner error handling
+        if(this.operators.length === 0) {
+            throw new Error();
+        }
+
+        return R.last(this.operators);
     }
 
     enforest() {
@@ -71,117 +80,99 @@ class Enforester {
             });
         }
 
-        this.term = null;
         return left;
     }
 
     enforestExpressionLoop() {
-        this.term = null; // TODO: Better naming
-        this.opCtx = {
+        // something is wrong if this.term isn't null
+        // or if this.operators isn't empty
+        // TODO: saner error handling
+        if(this.term !== null || this.operators.length !== 0) {
+            throw new Error();
+        }
+
+        // default operator
+        this.operators.push({
             prec: 0,
-            combine: x => x,
-            stack: []
-        };
-
-        do {
-            const term = this.enforestAssignmentExpression();
-            if(term === EXPR_LOOP_NO_CHANGE && this.opCtx.stack.length > 0) {
-                this.term = this.opCtx.combine(this.term);
-                const { prec, combine } = this.opCtx.stack.pop();
-                this.opCtx.prec = prec;
-                this.opCtx.combine = combine;
-            } else if(term === EXPR_LOOP_NO_CHANGE) {
-                break;
-            } else if(term === EXPR_LOOP_OPERATOR ||
-                      term === EXPR_LOOP_EXPANSION) {
-                this.term = null;
-            } else {
-                this.term = term;
-            }
-        } while(true);
-
-        return this.term;
-    }
-
-    enforestAssignmentExpression() {
-        const lookahead = this.peek();
-
-        // checking cases when this.term is nothing...
-
-        // numeric literals
-        if(this.term === null && this.isNumericLiteral(lookahead)) {
-            const term = this.advance();
-            if(term.token.value === Infinity) {
-                return new AST.LiteralInfinityExpression();
-            }
-
-            return new AST.LiteralNumericExpression({
-                value: term.token.value
-            });
-        }
-
-        // prefix unary ops
-        if(this.term === null && this.isOperator(lookahead)) {
-            return this.enforestUnaryExpression();
-        }
-
-        // check cases when this.term is something...
-
-        // binary ops
-        if(this.term && this.isOperator(lookahead)) {
-            return this.enforestBinaryExpression();
-        }
-
-        return EXPR_LOOP_NO_CHANGE;
-    }
-
-    enforestUnaryExpression() {
-        const operator = this.matchUnaryOperator();
-        this.opCtx.stack.push({
-            prec: this.opCtx.prec,
-            combine: this.opCtx.combine
+            combine: x => x
         });
 
-        // TODO: with custom operators, operators might be more than 14
-        this.opCtx.prec = 14;
-        this.opCtx.combine = operand => {
-            return new AST.UnaryExpression({
-                operand,
-                operator: operator.token.value
-            });
-        };
+        while(this.operators.length > 0) {
+            const lookahead = this.peek();
 
-        return EXPR_LOOP_OPERATOR;
+            // checking cases when this.term is nothing...
+            // numeric literals
+            if(this.term === null && this.isNumericLiteral(lookahead)) {
+                this.term = this.enforestNumericLiteral();
+                continue;
+            }
+
+            // prefix unary ops
+            if(this.term === null && this.isOperator(lookahead)) {
+                // TODO: check if unary operator
+                this.pushUnaryOperator();
+                continue;
+            }
+
+            // check cases when this.term is something...
+            // binary ops
+            // only if following operator is higher than current
+            // assuming that all operators are left-associated
+            // TODO: handle right-associated operators
+            if(this.term && this.isOperator(lookahead) &&
+               this.operator.prec < getOperatorPrec(lookahead)) {
+                // TODO: check if binary operator
+                this.pushBinaryOperator();
+                this.term = null;
+                continue;
+            }
+
+            this.term = this.operator.combine(this.term);
+            this.operators.pop();
+        }
+
+        const term = this.term;
+        this.term = null;
+
+        return term;
     }
 
-    enforestBinaryExpression() {
+    enforestNumericLiteral() {
+        const term = this.advance();
+        if(term.token.value === Infinity) {
+            return new AST.LiteralInfinityExpression();
+        }
+
+        return new AST.LiteralNumericExpression({
+            value: term.token.value
+        });
+    }
+
+    pushUnaryOperator() {
+        const operator = this.advance();
+        this.operators.push({
+            // TODO: custom unary operators might have different precedence
+            prec: 14,
+            combine: operand => new AST.UnaryExpression({
+                operand,
+                operator: operator.token.value
+            })
+        });
+    }
+
+    pushBinaryOperator() {
         const left = this.term;
-        const operator = this.peek();
-        const operatorPrec = getOperatorPrec(operator);
+        const operator = this.advance();
         // assuming that all operators are left-associated
         // TODO: handle right-associated operators
-        if(this.opCtx.prec < operatorPrec) {
-            this.opCtx.stack.push({
-                prec: this.opCtx.prec,
-                combine: this.opCtx.combine
-            });
-            this.opCtx.prec = operatorPrec;
-            this.opCtx.combine = right => {
-                return new AST.BinaryExpression({
-                    left,
-                    operator: operator.token.value,
-                    right
-                });
-            };
-            this.advance();
-            return EXPR_LOOP_OPERATOR;
-        } else {
-            const term = this.opCtx.combine(left);
-            const { prec, combine } = this.opCtx.stack.pop();
-            this.opCtx.prec = prec;
-            this.opCtx.combine = combine;
-            return term;
-        }
+        this.operators.push({
+            prec: getOperatorPrec(operator),
+            combine: right => new AST.BinaryExpression({
+                left,
+                operator: operator.token.value,
+                right
+            })
+        });
     }
 
     enforestBlockStatement() {
@@ -240,16 +231,6 @@ class Enforester {
         const term = this.advance();
         if(this.isParen(term)) {
             return term.inner;
-        }
-
-        // TODO: saner error handling
-        throw new Error();
-    }
-
-    matchUnaryOperator() {
-        const term = this.advance();
-        if(isUnaryOperator(term)) {
-            return term;
         }
 
         // TODO: saner error handling
